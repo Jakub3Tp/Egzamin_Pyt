e9148b0d2a0e57070fc67f438fecaa427bfe0956
Name <mymail@example.com>
2025-04-11 14:07:14 +0200

    11.04.2025

--- main.py
+++ main.py
@@ -3,7 +3,21 @@ class Urzadzenie:
         print(komunikat)
 
 
+class Pralka(Urzadzenie):
+    def __init__(self):
+        self.__numer_prania = 0 # Private
 
+    def ustaw_program(self, numer_prania):
+
+class Odkurzac(Urzadzenie):
+    def __init__(self):
+        self.__stan = False
+
+    def on(self):
+        ...
+
+    def off(self):
+        ...
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    pralka = Pralka()
+    odkurzac = Odkurzac()
